33822b49117015db5d9a98f179c0e95b380f8417
Stephen Rosen <sirosen@globus.org>
2019-04-02 12:49:12 +0000

    Update to be more readable, add license

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from __future__ import print_function, division
+from __future__ import division, print_function
+
+import argparse
+from collections import defaultdict
+from random import sample
 
 
 def get_ngrams(seq, n):
     ngrams = set()
-    current_ngram = ()
+    current_ngram = tuple()
     for s in seq:
         if len(current_ngram) < n - 1:
             current_ngram += (s,)
@@ -15,45 +19,42 @@ def get_ngrams(seq, n):
     return ngrams
 
 
-def dissociate(s, num_words, chunk_size):
-    from random import sample
-    seq = s.lower().split()
-    ngrams = get_ngrams(seq, chunk_size)
-    ngram_mapping = {}
+def dissociate(full_corpus, num_words, chunk_size):
+    ngrams = get_ngrams(full_corpus.lower().split(), chunk_size)
+    ngram_mapping = defaultdict(set)
     for ngram in ngrams:
         n_minus_one_gram = ngram[:-1]
-        if n_minus_one_gram not in ngram_mapping:
-            ngram_mapping[n_minus_one_gram] = set()
         ngram_mapping[n_minus_one_gram].add(ngram)
-    cur_num_words = chunk_size - 1
+
     current_ngram = sample(ngrams, 1)[0]
-    out_s = []
-    while cur_num_words < num_words:
-        out_s.append(current_ngram[-1])
+
+    def get_next_word():
+        nonlocal current_ngram
         tail = current_ngram[1:]
-        if tail not in ngram_mapping:
-            current_ngram = sample(ngrams, 1)[0]
+        if tail in ngram_mapping:
+            current_ngram = sample(ngram_mapping[tail], 1)[0]
         else:
-            current_ngram = sample(ngram_mapping[current_ngram[1:]], 1)[0]
-        cur_num_words += 1
-    out_s = ' '.join(out_s)
-    return out_s
+            current_ngram = sample(ngrams, 1)[0]
+        return current_ngram[-1]
 
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print(
-            "USAGE ERROR. bot.py filename num-words [chunk-size]",
-            file=sys.stderr)
-        sys.exit(1)
-    fname = sys.argv[1]
-    num_words = int(sys.argv[2])
-    chunk_size = 5
-    if len(sys.argv) > 3:
-        chunk_size = int(sys.argv[3])
+    return " ".join(get_next_word() for _ in range(num_words))
 
-    f = open(fname, 'r')
-    s = f.read()
-    f.close()
 
-    print(dissociate(s, num_words, chunk_size))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("NUM_WORDS", type=int)
+    parser.add_argument("FILENAMES", nargs="+")
+    parser.add_argument("--chunk-size", type=int, default=5)
+    args = parser.parse_args()
+
+    full_corpus = []
+    for fname in args.FILENAMES:
+        with open(fname, "r") as f:
+            full_corpus.append(f.read())
+    full_corpus = " ".join(full_corpus)
+
+    print(dissociate(full_corpus, args.NUM_WORDS, args.chunk_size))
+
+
+if __name__ == "__main__":
+    main()
